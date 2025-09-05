c03cac608d5f5157d8844049ac8cfc68737ce95d
IagoAntunes <iagoantunes.f@gmail.com>
2025-09-04 23:08:03 -0300

    feat: Add certificate image generation functionality and assets

--- backend/CertificateManagement/CertificateGenerator.Worker/CertificateGenerationConsumer.cs
+++ backend/CertificateManagement/CertificateGenerator.Worker/CertificateGenerationConsumer.cs
@@ -1,4 +1,5 @@
-﻿using CertificateManagement.Application.Requests;
+﻿using CertificateGenerator.Worker.Services;
+using CertificateManagement.Application.Requests;
 using CertificateManagement.Domain.Entities;
 using MassTransit;
 using MongoDB.Driver;
@@ -9,15 +10,20 @@ namespace CertificateGenerator.Worker
     {
         private readonly IMongoCollection<CertificateEntity> _collection;
         private readonly ILogger<CertificateGenerationRequest> _logger;
+        private readonly IHostEnvironment hostEnvironment;
+        private readonly CertificateImageGenerator _imageGenerator;
 
         public CertificateGenerationConsumer(
             IMongoDatabase database,
-            ILogger<CertificateGenerationRequest> logger
+            ILogger<CertificateGenerationRequest> logger,
+            IHostEnvironment hostEnvironment
             
         )
         {
             _collection = database.GetCollection<CertificateEntity>("certificates");
             this._logger = logger;
+            this.hostEnvironment = hostEnvironment;
+            _imageGenerator = new CertificateImageGenerator();
         }
 
         public async Task Consume(
@@ -27,21 +33,50 @@ namespace CertificateGenerator.Worker
             var certificateId = context.Message.CertificateId;
             _logger.LogInformation("Recebida solicitação para gerar certificado: {CertificateId}", certificateId);
 
-            _logger.LogInformation("Gerando imagem...");
-            await Task.Delay(5000);
-            var imageUrl = $"https://storage.example.com/images/{Guid.NewGuid()}.png";
-            _logger.LogInformation("Imagem gerada e salva em: {ImageUrl}", imageUrl);
+            var certificate = (await _collection.FindAsync(c => c.Id == certificateId)).FirstOrDefault();
+            if (certificate == null)
+            {
+                _logger.LogError("Certificado com ID {CertificateId} não encontrado.", certificateId);
+                return;
+            }
 
+            _logger.LogInformation("Gerando imagem para {StudentName}...", certificate.StudentFullName);
+            using var imageStream = await _imageGenerator.GenerateImageAsync(
+                certificate.StudentFullName,
+                string.Join(", ", certificate.Activities),
+                certificate.CreatedAt
+            );
+
+            var imagesDirectory = Path.Combine(hostEnvironment.ContentRootPath, "Images");
+
+            Directory.CreateDirectory(imagesDirectory);
+
+            var fileName = $"{Guid.NewGuid()}.png";
+            var filePath = Path.Combine(imagesDirectory, fileName);
+
+            _logger.LogInformation("Salvando imagem em disco no caminho: {FilePath}", filePath);
+
+           
+            await using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                imageStream.Position = 0; 
+                await imageStream.CopyToAsync(fileStream);
+            }
+
+            _logger.LogInformation("Imagem salva com sucesso.");
+
+
+            // Atualiza o MONGODB com a imagem
             var filter = Builders<CertificateEntity>.Filter.Eq(c => c.Id, certificateId);
             var update = Builders<CertificateEntity>.Update.Set(c => c.Image, new CertificateImage
             {
-                Path = imageUrl,
-                FileName = Path.GetFileName(imageUrl),
+                Path = filePath, 
+                FileName = fileName,
                 ContentType = "image/png"
             });
 
             await _collection.UpdateOneAsync(filter, update);
-            _logger.LogInformation("Documento do certificado {CertificateId} atualizado com a imagem.", certificateId);
+            _logger.LogInformation("--> Documento do certificado {CertificateId} atualizado com o caminho do arquivo local.", certificateId);
         }
     }
 }
---
+++ backend/CertificateManagement/CertificateGenerator.Worker/Services/CertificateImageGenerator.cs
@@ -0,0 +1,66 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.Fonts;
+
+namespace CertificateGenerator.Worker.Services
+{
+    public class CertificateImageGenerator
+    {
+        public async Task<Stream> GenerateImageAsync(
+            string studentName,
+            string activities,
+            DateTime generationDate
+            )
+        {
+            var fontCollection = new FontCollection();
+            var fontFamily = fontCollection.Add("Assets/DancingScriptVariableFont.ttf");
+            var interFontFamily = fontCollection.Add("Assets/InterVariableFont.ttf");
+
+            using (var image = await Image.LoadAsync("Assets/certificate_template.png"))
+            {
+                // StudentName
+                var centerX = image.Width / 2;
+                var studentNameFont = fontFamily.CreateFont(50, FontStyle.Bold);
+                var studentNameOptions = new RichTextOptions(studentNameFont)
+                {
+                    Origin = new PointF(centerX, 210), 
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    WrappingLength = image.Width - 200
+                };
+
+                // Activities
+                var activitiesFont = interFontFamily.CreateFont(14, FontStyle.Regular);
+                var activitiesOptions = new RichTextOptions(activitiesFont)
+                {
+                    Origin = new PointF(centerX, 340),
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    WrappingLength = image.Width - 300
+                };
+
+                // Date
+                var dateFont = interFontFamily.CreateFont(12, FontStyle.Bold);
+                var dateOptions = new RichTextOptions(dateFont)
+                {
+                    Origin = new PointF(centerX, 450), 
+                    HorizontalAlignment = HorizontalAlignment.Center
+                };
+                var formattedDate = generationDate.ToString("dd 'de' MMMM 'de' yyyy");
+
+                image.Mutate(ctx =>
+                {
+                    ctx.DrawText(studentNameOptions, studentName, Color.FromRgb(59,90,194)); 
+                    ctx.DrawText(activitiesOptions, activities, Color.FromRgb(18,22,33));
+                    ctx.DrawText(dateOptions, formattedDate, Color.FromRgb(18,22,33));
+                });
+
+                
+                var outputStream = new MemoryStream();
+                await image.SaveAsPngAsync(outputStream);
+                outputStream.Position = 0; 
+
+                return outputStream;
+            }
+        }
+    }
+}
